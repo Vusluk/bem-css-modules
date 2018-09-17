d17c6620855591a5f3ec366c7792aaa0e5f30463
Станислав Дубровский <vusluk@gmail.com>
2018-09-18 01:48:14 +0800

    change indentation to 2 spaces; add trim to result string

--- src/bem-css-modules.js
+++ src/bem-css-modules.js
@@ -11,139 +11,137 @@ const settings = {throwOnError: false};
  * @return {String}
  */
 function block(cssModule, name, elementParam, modsParam, statesParam) {
-    const isElementAsModes = elementParam && typeof elementParam === 'object';
-    const mods = isElementAsModes ? elementParam : modsParam;
-    const states = isElementAsModes ? modsParam : statesParam;
-    const element = isElementAsModes ? '' : elementParam;
+  const isElementAsModes = elementParam && typeof elementParam === 'object';
+  const mods = isElementAsModes ? elementParam : modsParam;
+  const states = isElementAsModes ? modsParam : statesParam;
+  const element = isElementAsModes ? '' : elementParam;
 
-    const baseBlock = element ? `${name}__${element}` : name;
-    let result = cssModule[baseBlock] || '';
+  const baseBlock = element ? `${name}__${element}` : name;
+  let result = cssModule[baseBlock] || '';
 
-    if (isDev) {
-        if (!result && !mods) {
-            const message = `There is no ${name}__${element} in cssModule`;
+  if (isDev) {
+    if (!result && !mods) {
+      const message = `There is no ${name}__${element} in cssModule`;
 
-            if (settings.throwOnError) {
+      if (settings.throwOnError) {
+        throw Error(message);
+      } else {
+        console.warn(message);
+        return '';
+      }
+    }
+  }
+
+  if (mods) {
+    result = Object.keys(mods)
+      .reduce((acc, next) => {
+        const modValue = mods[next];
+
+        let mod;
+
+        if (typeof modValue === 'boolean') {
+          if (isDev) {
+            if (!(`${baseBlock}_${next}` in cssModule)) {
+              const message = `There is no ${baseBlock}_${next} in cssModule`;
+
+              if (settings.throwOnError) {
                 throw Error(message);
-            } else {
+              } else {
                 console.warn(message);
                 return '';
+              }
             }
+          }
+
+          if (modValue) {
+            mod = cssModule[`${baseBlock}_${next}`];
+          } else {
+            return acc;
+          }
+        } else {
+          if (isDev) {
+            if (!(`${baseBlock}_${next}_${mods[next]}` in cssModule)) {
+              const message = `There is no ${baseBlock}_${next}_${mods[next]} in cssModule`;
+
+              if (settings.throwOnError) {
+                throw Error(message);
+              } else {
+                console.warn(message);
+                return '';
+              }
+            }
+          }
+
+          mod = cssModule[`${baseBlock}_${next}_${mods[next]}`];
         }
-    }
 
-    if (mods) {
-        result = Object.keys(mods)
-            .reduce((acc, next) => {
-                const modValue = mods[next];
+        return `${acc} ${mod}`;
+      }, result);
+  }
 
-                let mod;
+  if (states) {
+    result = Object.keys(states)
+      .reduce((acc, next) => {
+        if (!states[next]) {
+          return acc;
+        }
 
-                if (typeof modValue === 'boolean') {
-                    if (isDev) {
-                        if (!(`${baseBlock}_${next}` in cssModule)) {
-                            const message = `There is no ${baseBlock}_${next} in cssModule`;
+        const state = cssModule[`is-${next}`];
 
-                            if (settings.throwOnError) {
-                                throw Error(message);
-                            } else {
-                                console.warn(message);
-                                return '';
-                            }
-                        }
-                    }
+        if (!state) {
+          const message = `There is no is-${next} in cssModule`;
 
-                    if (modValue) {
-                        mod = cssModule[`${baseBlock}_${next}`];
-                    } else {
-                        return acc;
-                    }
-                } else {
-                    if (isDev) {
-                        if (!(`${baseBlock}_${next}_${mods[next]}` in cssModule)) {
-                            const message = `There is no ${baseBlock}_${next}_${mods[next]} in cssModule`;
+          if (settings.throwOnError) {
+            throw Error(message);
+          } else {
+            console.warn(message);
+            return '';
+          }
+        }
 
-                            if (settings.throwOnError) {
-                                throw Error(message);
-                            } else {
-                                console.warn(message);
-                                return '';
-                            }
-                        }
-                    }
+        return `${acc} ${state}`;
+      }, result);
+  }
 
-                    mod = cssModule[`${baseBlock}_${next}_${mods[next]}`];
-                }
-
-                return `${acc} ${mod}`;
-            }, result);
-    }
-
-    if (states) {
-        result = Object.keys(states)
-            .reduce((acc, next) => {
-                if (!states[next]) {
-                    return acc;
-                }
-
-                const state = cssModule[`is-${next}`];
-
-                if (!state) {
-                    const message = `There is no is-${next} in cssModule`;
-
-                    if (settings.throwOnError) {
-                        throw Error(message);
-                    } else {
-                        console.warn(message);
-                        return '';
-                    }
-                }
-
-                return `${acc} ${state}`;
-            }, result);
-    }
-
-    return result;
+  return result.trim();
 }
 
 const regExpClearBEM = /__.*/g;
 
 const extractModuleName = (cssModule) => {
-    if (isDev) {
-        if (!cssModule || typeof cssModule !== 'object' || Array.isArray(cssModule)) {
-            const message = 'cssModule object should be a Object with keys';
+  if (isDev) {
+    if (!cssModule || typeof cssModule !== 'object' || Array.isArray(cssModule)) {
+      const message = 'cssModule object should be a Object with keys';
 
-            if (settings.throwOnError) {
-                throw Error(message);
-            } else {
-                console.warn(message);
-                return '';
-            }
-        }
+      if (settings.throwOnError) {
+        throw Error(message);
+      } else {
+        console.warn(message);
+        return '';
+      }
     }
+  }
 
-    const [name] = Object.keys(cssModule);
+  const [name] = Object.keys(cssModule);
 
-    if (isDev) {
-        if (!name) {
-            const message = 'cssModule has no keys';
+  if (isDev) {
+    if (!name) {
+      const message = 'cssModule has no keys';
 
-            if (settings.throwOnError) {
-                throw Error(message);
-            } else {
-                console.warn(message);
-                return '';
-            }
-        }
+      if (settings.throwOnError) {
+        throw Error(message);
+      } else {
+        console.warn(message);
+        return '';
+      }
     }
+  }
 
-    return name.replace(regExpClearBEM, '');
+  return name.replace(regExpClearBEM, '');
 };
 
-const bem = (cssModule, name) =>
-    block.bind(null, cssModule, name || extractModuleName(cssModule));
+const bem = (cssModule, name) => block.bind(null, cssModule, name || extractModuleName(cssModule));
 
-bem.setSettings = (newSettings) =>
-    Object.assign(settings, newSettings);
+bem.setSettings = (newSettings) => Object.assign(settings, newSettings);
 
 export default bem;
